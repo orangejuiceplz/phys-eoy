cba1ba136fdddb15841e300a0f32e44fa1688275
λamdashi <orangejuiceplz12321@gmail.com>
2026-05-12 18:46:03 -0400

    calculate drag and integrate

---
+++ src/flight_math.c
@@ -0,0 +1,51 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 orangejuiceplz
+ * CREATED on 5/12/26 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <math.h>
+#include "../include/flight_math.h"
+
+double calculate_drag(double velocity, double drag_coefficient, double area) {
+    // Force_drag = 1/2 * rho * v^2 * dC * A
+    return 0.5 * air_density * pow(velocity, 2) * drag_coefficient * area;
+}
+
+// stupid integrator
+void update(Lander *l, double dt) {
+    const double force_gravity = l->mass * g;
+    double force_drag = calculate_drag(l->velocity, l->drag_coefficient, l->area);
+
+    if (l->velocity > 0) {
+        force_drag = -force_drag;
+    }
+
+    const double net_force = force_drag - force_gravity;
+
+    l->acceleration = net_force / l->mass; //a = sum(F)/m
+    l->velocity += l->acceleration * dt;
+    l->altitude += l->velocity * dt;
+}
+
+
+
