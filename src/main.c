16a50d0139d9b5ee6ffbf44f490b425c0e46f51f
λamdashi <orangejuiceplz12321@gmail.com>
2026-05-12 19:32:25 -0400

    add base lander

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-
+#include "../include/flight_math.h"
 int main(void) {
-    printf("Hello, World!\n");
+    Lander lander = {
+        .mass = 0,
+        .velocity = 0,
+        .drag_coefficient = 0,
+        .altitude = 0,
+        .area = 0,
+        .acceleration = 0,
+        .deployment_altitude = 0,
+        .name= "sky-lander",
+        .deployed = 0
+    };
+    while ()
     return 0;
 }
